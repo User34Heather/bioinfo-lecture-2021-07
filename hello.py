fb3e92ec5dfdda8a3dc10ab8fd3515f534bab218
User34Heather <chelly929@naver.com>
2021-07-05 14:59:41 +0900

    #1 get name

--- hello.py
+++ hello.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python
 
-print("Hello Bioinformatics")
+import sys
+
+print(sys.argv)
+# sys.exit
+
+name = sys.argv[1]   #리스트에서 1번째 리스트를 가져와서 argument에 넣는다.
+name2 = sys.argv[2]
+
+print(f"Hello {name}")
+print(f"hello {name2}")
 
